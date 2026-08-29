3f0a3dba7ed610cd6c45996043be28576b15c9bc
Михаил Королев <mihail_korolev1111@mail.ru>
2026-08-29 11:58:06 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import one from './assets/1.jpg'
+import two from './assets/2.jpg'
+import three from './assets/3.jpg'
+import four from './assets/4.jpg'
+import five from './assets/5.jpg'
+import './App.css'
+
+function App() {
+    return (
+        <>
+            <img src={one}/>
+            <img src={two}/>
+            <img src={three}/>
+            <img src={four}/>
+            <img src={five}/>
+        </>
+    )
+}
+
+export default App
